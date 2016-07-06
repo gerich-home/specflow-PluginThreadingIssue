71301f098d9c37e2d5d3c0629480d2b32864ada1
sergey-gerasimov <sergey.gerasimov@exigenservices.com>
2016-07-06 11:15:40 +0300

    Some features

---
+++ PluginThreadingIssue.Specs/StepDefinitions.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace PluginThreadingIssue.Specs
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            LongAction();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            LongAction();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            LongAction();
+        }
+
+        private static void LongAction()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
